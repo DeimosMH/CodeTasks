17da13ea75343d295ab1302bbda4793a711efb6e
Maciej Hejlasz <maciejhejlasz@gmail.com>
2023-06-13 02:11:02 +0200

    Clean, remove not necessary files

--- start.bat
+++
@@ -1,2 +0,0 @@
-start "DailyCodingHabit.code-workspace" "C:\Users\Deimos\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-start "./.Books\C++_Primer_Plus,_Sixth_Edition,_2012,_Stephan_Prata.pdf" "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
