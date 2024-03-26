546fc0b319f337a7388af975ed01df3d6b707926
DeimosMH <maciejhejlasz@gmail.com>
2024-03-26 18:24:43 +0100

    FCC_clean_projects

---
+++ _helpers/temp.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+// Function to calculate the nth Fibonacci number
+int fibonacci(int n)
+{
+}
+
+int main()
+{
+    // Hard coded input 
+
+    // Function call
+    cout << "The " << n << "th Fibonacci number is: " << fibonacci(n) << endl;
+
+    return 0;
+}
+
